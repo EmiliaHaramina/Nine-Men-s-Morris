3b6d03c20aff71379285a9580624cf432e19c174
Emilia Haramina <emilia.haramina1@gmail.com>
2024-09-23 00:05:28 +0200

    Sound effects initialized in SettingsManager

--- Assets/Scripts/General/SettingsManager.cs
+++ Assets/Scripts/General/SettingsManager.cs
@@ -7,6 +7,10 @@ public class SettingsManager : MonoBehaviour
     [SerializeField] private MusicController musicController;
     // Background music volume controller
     [SerializeField] private MusicVolumeController musicVolumeController;
+    // Sound effects controller
+    [SerializeField] private SoundEffectsController soundEffectsController;
+    // Sound effects volume controller
+    [SerializeField] private SoundEffectsVolumeController soundEffectsVolumeController;
 
     // Initializes the player prefs and the sound and volume for the background
     // music
@@ -19,5 +23,9 @@ public class SettingsManager : MonoBehaviour
         musicController.Initialize();
         // Initializes background music volume
         musicVolumeController.Initialize();
+        // Initializes sound effects
+        soundEffectsController.Initialize();
+        // Initializes sound effects volume
+        soundEffectsVolumeController.Initialize();
     }
 }
